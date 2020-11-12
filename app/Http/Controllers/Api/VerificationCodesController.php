b9e19cdb3732aced4b4f097ddeb3e17313ef6eb3
Luke <328872037@qq.com>
2020-11-12 10:29:49 +0000

    图片验证码

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -6,18 +6,34 @@ use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Overtrue\EasySms\EasySms;
 use App\Http\Requests\Api\VerificationCodeRequest;
+use Illuminate\Auth\AuthenticationException;
 
 class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request, EasySms $easySms){
-        $phone = $request->phone;
+        $captchaData = \Cache::get($request->captcha_key);
 
-        //生成4位随机数，左侧补0
-        $code = str_pad(random_int(1,9999),4,0,STR_PAD_LEFT);
+        if (!$captchaData){
+            abort(403,'图片验证码已失效');
+        }
+
+        if (!hash_equals($captchaData['code'],$request->captcha_code)){
+            //验证错误就清除验证码
+            \Cache::forget($request->captcha_key);
+            throw new AuthenticationException('验证码错误');
+        }
+
+        $phone = $captchaData['phone'];
+
+
 
         if(!app()->environment('production')){
             $code = '1234';
         }else {
+            //生成4位随机数，左侧补0
+            $code = str_pad(random_int(1,9999),4,0,STR_PAD_LEFT);
+
+
             try {
                 $result = $easySms->send($phone,[
                     'template' => config('easysms.gateways.aliyun.templates.register'),
@@ -27,7 +43,7 @@ class VerificationCodesController extends Controller
                 ]);
             } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
                 $message = $exception->getException('aliyun')->getMessage();
-                abort( 500,$message ?: '短信发送失败');
+                abort( 500,$message ?: '短信发送异常');
             }
         }
 
@@ -36,6 +52,8 @@ class VerificationCodesController extends Controller
         $expiredAt = now()->addMinutes(5);
         // 缓存验证码 5 分钟过期
         \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+        // 清除图片验证码缓存
+        \Cache::forget($request->captcha_key);
 
         return response()->json([
             'key' => $key,
