1ec0acaa81319628ebc2f291f7d0a9ce0bc9493a
Luke <328872037@qq.com>
2020-11-14 09:50:28 +0000

    编辑用户信息

--- routes/api.php
+++ routes/api.php
@@ -63,6 +63,12 @@ Route::prefix('v1')
                     // 当前登录的用户信息
                     Route::get('user','UsersController@me')
                         ->name('user.me');
+                    // 编辑登录用户信息
+                    Route::patch('user','UsersController@update')
+                        ->name('user.update');
+                    // 上传图片
+                    Route::post('images','ImagesController@store')
+                        ->name('images.store');
                 });
             });
 
